180a05752e74f77c5f5176643f9f9cd329d2a4d0
ferris <jake@fusetools.com>
2016-06-10 00:03:54 +0200

    Generalized alg practice and finished alg views

--- app/oll-practice.component.ts
+++ app/oll-practice.component.ts
@@ -1,83 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Algorithm, AlgorithmType } from './algorithm';
-import { Database } from './database.service';
+import { AlgorithmType } from './algorithm';
 
-import { AlgorithmComponent } from './algorithm.component';
-
-enum State {
-    Unitialized,
-    Idle,
-    Countdown,
-    Running
-}
+import { AlgorithmPracticeComponent } from './algorithm-practice.component';
 
 @Component({
     selector: 'oll-practice',
     templateUrl: 'app/oll-practice.component.html',
-    directives: [AlgorithmComponent],
-    host: {
-        '(document:keydown)': 'keydown($event)'
-    }
+    directives: [AlgorithmPracticeComponent]
 })
-export class OllPracticeComponent implements OnInit {
-    algorithms: Algorithm[];
-
-    currentAlgorithm: Algorithm;
-    displayValue = "ready";
-    showAlgorithm = false;
-
-    private state = State.Unitialized;
-    private startTime;
-    private interval;
-
-    constructor(private database: Database) { }
-
-    ngOnInit() {
-        this.database.getAlgorithms().then(
-            algorithms => {
-                this.algorithms = algorithms.filter(
-                    x => x.type == AlgorithmType.Oll);
-
-                this.initialize();
-            });
-    }
-
-    private initialize() {
-        this.state = State.Idle;
-    }
-
-    private keydown(event: KeyboardEvent) {
-        if (event.keyCode == 32) {
-            switch (this.state) {
-            case State.Idle:
-                this.state = State.Countdown;
-                this.currentAlgorithm = null;
-                this.showAlgorithm = false;
-                this.displayValue = "3";
-                setTimeout(() => {
-                    this.displayValue = "2";
-                    setTimeout(() => {
-                        this.currentAlgorithm = this.algorithms[Math.floor(Math.random() * this.algorithms.length)];
-                        this.displayValue = "1";
-                        setTimeout(() => {
-                            this.state = State.Running;
-                            this.startTime = Date.now();
-                            this.interval = setInterval(() => {
-                                let elapsed = new Date(Date.now() - this.startTime);
-                                this.displayValue = elapsed.getSeconds() + "." + elapsed.getMilliseconds();
-                            }, 0);
-                        }, 1000);
-                    }, 1000);
-                }, 1000);
-                break;
-
-            case State.Running:
-                this.state = State.Idle;
-                clearInterval(this.interval);
-                this.showAlgorithm = true;
-                break;
-            }
-        }
-    }
+export class OllPracticeComponent {
+    algorithmType = AlgorithmType.Oll;
 }
