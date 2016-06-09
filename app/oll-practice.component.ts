4aade9c7c2ec401246c2e6a4dfad4301480019d7
ferris <jake@fusetools.com>
2016-06-09 23:00:09 +0200

    Super rudimentary alg practice starting to work

--- app/oll-practice.component.ts
+++ app/oll-practice.component.ts
@@ -3,28 +3,76 @@ import { Component, OnInit } from '@angular/core';
 import { Algorithm, AlgorithmType } from './algorithm';
 import { Database } from './database.service';
 
+enum State {
+    Unitialized,
+    Idle,
+    Countdown,
+    Running
+}
+
 @Component({
     selector: 'oll-practice',
-    templateUrl: 'app/oll-practice.component.html'
+    templateUrl: 'app/oll-practice.component.html',
+    host: {
+        '(document:keydown)': 'keydown($event)'
+    }
 })
 export class OllPracticeComponent implements OnInit {
     algorithms: Algorithm[];
+
     currentAlgorithm: Algorithm;
+    displayValue = "ready";
+    moves = "";
+
+    private state = State.Unitialized;
+    private startTime;
+    private interval;
 
     constructor(private database: Database) { }
 
     ngOnInit() {
-        this.getAlgorithms();
-    }
-
-    getAlgorithms() {
         this.database.getAlgorithms().then(
             algorithms => {
                 this.algorithms = algorithms.filter(
                     x => x.type == AlgorithmType.Oll);
 
-                // TODO: Randomize
-                this.currentAlgorithm = this.algorithms[0];
+                this.initialize();
             });
+    }
+
+    private initialize() {
+        this.state = State.Idle;
+    }
+
+    private keydown(event: KeyboardEvent) {
+        if (event.keyCode == 32) {
+            switch (this.state) {
+            case State.Idle:
+                this.state = State.Countdown;
+                this.currentAlgorithm = this.algorithms[Math.floor(Math.random() * this.algorithms.length)];
+                this.moves = "";
+                this.displayValue = "3";
+                setTimeout(() => {
+                    this.displayValue = "2";
+                    setTimeout(() => {
+                        this.displayValue = "1";
+                        setTimeout(() => {
+                            this.state = State.Running;
+                            this.startTime = Date.now();
+                            this.interval = setInterval(() => {
+                                this.displayValue = Date.now() - this.startTime;
+                            }, 0);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+                break;
+
+            case State.Running:
+                this.state = State.Idle;
+                clearInterval(this.interval);
+                this.moves = this.currentAlgorithm.moves;
+                break;
+            }
+        }
     }
 }
