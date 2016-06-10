a5294022608953c789ec890e36ffb30c51dc5dc1
ferris <jake@fusetools.com>
2016-06-10 02:26:45 +0200

    closer..?

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,10 +5,10 @@
 (function(global) {
     // map tells the System loader where to look for things
     var map = {
-        'app':                        'assistant/app', // 'dist',
-        '@angular':                   'assistant/lib/@angular',
-        'angular2-in-memory-web-api': 'assistant/lib/angular2-in-memory-web-api',
-        'rxjs':                       'assistant/lib/rxjs'
+        'app':                        'app', // 'dist',
+        '@angular':                   'lib/@angular',
+        'angular2-in-memory-web-api': 'lib/angular2-in-memory-web-api',
+        'rxjs':                       'lib/rxjs'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
